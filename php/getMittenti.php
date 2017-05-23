6c9f389b7f48b04d12e343282dadcb5cbafddac9
GiacomoFava <giacomofava93@gmail.com>
2017-05-23 18:14:34 +0200

    prova

--- php/getMittenti.php
+++ php/getMittenti.php
@@ -6,15 +6,11 @@
  * and open the template in the editor.
  */
 
-$servername = "mysql://mydb:3306/";
-$username = "userM6Q";
-$password = "Fdk40EuttXcXKiH0";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
+$dbopts = parse_url(getenv('postgres://isnivxsqqafmbk:7d30c696b3661d3c70af1d34cb6634c2ceae487abb8601ef6f9783ce8b2a231f@ec2-54-83-26-65.compute-1.amazonaws.com:5432/d7tmei47o4mnnr'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
