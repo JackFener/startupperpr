637b308875288cbe471111ab8a048215a4a1c3c3
GiacomoFava <giacomofava93@gmail.com>
2017-05-23 18:09:28 +0200

    composer add

--- php/getMittenti.php
+++ php/getMittenti.php
@@ -5,18 +5,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-use Herrera\Pdo\PdoServiceProvider;
-/*
-$host="ec2-54-83-26-65.compute-1.amazonaws.com";
-$dbname="d7tmei47o4mnnr";
-$user="isnivxsqqafmbk";
-$pass="7d30c696b3661d3c70af1d34cb6634c2ceae487abb8601ef6f9783ce8b2a231f";
-*/
-$dbopts = parse_url(getenv('postgres://isnivxsqqafmbk:7d30c696b3661d3c70af1d34cb6634c2ceae487abb8601ef6f9783ce8b2a231f@ec2-54-83-26-65.compute-1.amazonaws.com:5432/d7tmei47o4mnnr'));
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
-               array(
-                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' .$dbopts["port"],
-                   'pdo.username' => $dbopts["user"],
-                   'pdo.password' => $dbopts["pass"]
-               )
-);
+
+$servername = "mysql://mydb:3306/";
+$username = "userM6Q";
+$password = "Fdk40EuttXcXKiH0";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
